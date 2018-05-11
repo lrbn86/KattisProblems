42d0340a5ef11bb3d5448d67f8198209c83a1e17
Brandon Nguyen <lrbn86@gmail.com>
2018-05-11 17:42:20 -0400

    Added BatterUp problem

---
+++ BatterUp/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	int n;
+	std::cin >> n;
+	int at_bat[n];
+	for (int i = 0; i < n; i++) {
+		std::cin >> at_bat[i];
+	}
+	double result = 0;
+	double sum = 0;
+	double numOfNegatives = 0;
+	for (int i = 0; i < n; i++) {
+		if (at_bat[i] == -1) {
+			numOfNegatives++;
+		} else {
+			sum += at_bat[i];
+		}
+
+	}
+	result = (sum / (n - numOfNegatives));
+	std::cout << result << std::endl;
+	return 0;
+}
